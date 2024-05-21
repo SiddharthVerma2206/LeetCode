75bb76410ddfa8013c28a49f4a0ea78f05857f0e
Siddharth Verma <sidverma2206@gmail.com>
2024-05-21 15:54:17 +0530

    Day 15

---
+++ SlidingWindow/BuynSellStocks.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxpro=0;
+        int minprice=prices[0];
+        for(int i=1 ; i<prices.size() ; i++){
+            maxpro=max(maxpro ,prices[i]-minprice);
+            minprice=min(minprice , prices[i]);
+        }
+        return maxpro;
+    }
+};
