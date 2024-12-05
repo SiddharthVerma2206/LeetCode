df6f16046aac72b594d2842226d3c9edc358fc41
Siddharth <sidverma2206@gmail.com>
2024-12-05 22:26:07 +0530

    Day 140

---
+++ LC/Easy/ReversePrefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i=0;
+        while(i<word.size()){
+            if(word[i]==ch){
+                break;
+            }
+            i++;
+        }
+        if(i!=word.size()){
+            reverse(word.begin(),word.begin()+i+1);
+        }    
+        return word;
+    }
+};
