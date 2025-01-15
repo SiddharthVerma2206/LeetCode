fad46124ee814c1090df719cb68fab93c094b1eb
Siddharth <sidverma2206@gmail.com>
2025-01-15 20:07:16 +0530

    Day 178

---
+++ LC/Medium/SingleElementInArray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        unordered_map<int , int>mp;
+        for(int num : nums){
+            mp[num]++;
+        }
+        for(auto i : mp){
+            if(i.second == 1){
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
