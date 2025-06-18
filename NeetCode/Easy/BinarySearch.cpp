84ac5a343a06ec31f614b56741f2b13d8ba08be8
SiddharthVerma2206 <sidverma2206@gmail.com>
2025-06-18 20:42:42 +0530

    Day 236

--- NeetCode/Easy/BinarySearch.cpp
+++ NeetCode/Easy/BinarySearch.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size() - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                return mid; 
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        int left = 0 , right = nums.size() - 1;
+        while(left<=right){
+            int mid = (left + right)/2;
+            if (target == nums[mid]){
+                return mid;
+            }
+            else if (nums[mid] < target){
+                left = mid +1;
+            }
+            else{
+                right = mid - 1;
             }
         }
-
-        return -1; 
+        return -1;
     }
 };
